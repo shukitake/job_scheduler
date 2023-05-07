93cd954f73982239e6e1fd45e82141156652575a
Shuki Takeuchi <113006420+shukitake@users.noreply.github.com>
2023-05-07 20:26:19 +0900

    リファクタリング

--- model/model_3.py
+++ model/model_3.py
@@ -1,35 +1,89 @@
 """最適化のモデラーをインポート"""
+import datetime
+import os
+
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
 import pulp
 from utils.log import LoggerUtil
 
 
 class ProdPlan:
-    """
-    モデルを作成するクラス
+    """ジョブスケジューリング最適化問題をモデル化するクラス
 
-    Attributes
-    ----------
-    J : list
-        ジョブの集合
-    P : dict
-        ジョブの処理時間の集合
-    W : dict
-        ジョブの重要度の集合
-    R : dict
-        ジョブのリリース時間の集合
-    T : int
-        時間の集合。最大値はリリース時間の最大値 + 処理時間の合計
+    ジョブ処理時間、ジョブリリース時間、ジョブ重みを考慮して、ジョブスケジューリング問題を最適化するための最適化モデルを作成します。
+
+    Attributes:
+    PROCESS_FNAME (str)：ジョブ処理時間が含まれるファイルの名前
+    WEIGHTS_FNAME (str)：ジョブの重みが含まれるファイルの名前
+    RELEASE_FNAME (str)：ジョブリリース時間が含まれるファイルの名前
+
+    Args:
+    list_j (list)：ジョブIDのリスト
+    dict_p (dict)：ジョブ処理時間の辞書（キー：ジョブID、値：処理時間）
+    dict_w (dict)：ジョブの重みの辞書（キー：ジョブID、値：重み）
+    dict_r (dict)：ジョブリリース時間の辞書（キー：ジョブID、値：リリース時間）
+
+    methods:
+    pandas_read(indpath, nums)：pandasを使用してcsvファイルからジョブデータを読み込みます
+    list_to_dict(list_j, list_p, list_w, list_r)：リストからデータを辞書に変換します
+    modeling()：最適化モデルを構築し、制約を設定します
+    solve()：最適化モデルを解きます
+    visualize()：結果をガントチャートにして可視化します
+    get_time()：処理時間を取得します
+    get_model_info()：モデルの情報を取得します
+    get_result()：結果の情報を取得します
     """
 
-    def __init__(self, jobs, time_p, weights, time_r) -> None:
+    PROCESS_FNAME = "process.csv"
+    WEIGHTS_FNAME = "weights.csv"
+    RELEASE_FNAME = "release.csv"
+
+    @classmethod
+    def pandas_read(cls, indpath, nums):
+        """
+        pandasでデータを読み込む関数
+        """
+        time_p_fpath = os.path.join(indpath, cls.PROCESS_FNAME)
+        weights_fpath = os.path.join(indpath, cls.WEIGHTS_FNAME)
+        release_fpath = os.path.join(indpath, cls.RELEASE_FNAME)
+
+        time_p_dfm = pd.read_csv(time_p_fpath)
+        weights_dfm = pd.read_csv(weights_fpath)
+        release_dfm = pd.read_csv(release_fpath)
+
+        list_j = list(range(1, nums + 1))
+        # dfmからnums数だけ取り出してリストに変換
+        list_p = time_p_dfm["p"].head(nums).to_list()
+        list_w = weights_dfm["w"].head(nums).to_list()
+        list_r = release_dfm["r"].head(nums).to_list()
+
+        return list_j, list_p, list_w, list_r
+
+    @classmethod
+    def list_to_dict(cls, list_j, list_p, list_w, list_r):
+        """
+        データを辞書型に変換する関数
+        """
+        dict_p = dict()
+        dict_w = dict()
+        dict_r = dict()
+        for v_j, v_p, v_w, v_r in zip(list_j, list_p, list_w, list_r):
+            dict_p[v_j] = v_p
+            dict_w[v_j] = v_w
+            dict_r[v_j] = v_r
+        return dict_p, dict_w, dict_r
+
+    def __init__(self, list_j, dict_p, dict_w, dict_r):
         # ロガーの作成
         self.logger = LoggerUtil().get_logger(__name__)
 
         # 辞書型にして入力
-        self.jobs = jobs
-        self.dict_p = time_p
-        self.dict_w = weights
-        self.dict_r = time_r
+        self.jobs = list_j
+        self.dict_p = dict_p
+        self.dict_w = dict_w
+        self.dict_r = dict_r
         self.times = list(
             range(1, max(self.dict_r.values()) + sum(self.dict_p.values()))
         )
@@ -41,20 +95,6 @@ class ProdPlan:
         # Result
         self.status = -1
         self.objective = -1
-
-    @classmethod
-    def make_data(cls, jobs, time_p, weights, time_r):
-        """
-        データを辞書型に変換する関数
-        """
-        dict_p = dict()
-        dict_w = dict()
-        dict_r = dict()
-        for j, p_value, w_value, r_value in zip(jobs, time_p, weights, time_r):
-            dict_p[j] = p_value
-            dict_w[j] = w_value
-            dict_r[j] = r_value
-        return dict_p, dict_w, dict_r
 
     def modeling(self):
         """
@@ -138,13 +178,99 @@ class ProdPlan:
         # 最適解
         self.logger.info(f"最適値 : {pulp.value(self.model.objective)}")
 
-    def show_result(self):
+    def visualize(self):
         """
-        結果を表示する関数
+        結果をガントチャートで表示する関数
+
+        z_jtの値が1のとき、ジョブjは時刻tに処理されている
         """
-        # 結果の表示
-        for j in self.jobs:
-            for t in self.times:
-                if self.var_z[j, t].value() == 1:
-                    self.logger.info(f"ジョブ{j}は時刻{t}に処理される")
-        return
+        # ガントチャートの作成
+        # ジョブの開始、終了の数値は日にちの値とする
+        # 　ジョブの開始日は2023年1月1日とする
+        gantt_chart_df = pd.DataFrame(
+            {
+                "Task": self.jobs,
+                "Start": [
+                    datetime.datetime(2023, 1, 1, 0, 0, 0) + datetime.timedelta(days=t)
+                    for t in [
+                        pulp.value(
+                            pulp.lpSum(
+                                [
+                                    t_dash * self.var_z[j, t_dash].value()
+                                    for t_dash in self.times
+                                ]
+                            )
+                        )
+                        for j in self.jobs
+                    ]
+                ],
+                "Finish": [
+                    datetime.datetime(2023, 1, 1, 0, 0, 0) + datetime.timedelta(days=t)
+                    for t in [
+                        pulp.value(
+                            pulp.lpSum(
+                                [
+                                    t_dash * self.var_z[j, t_dash].value()
+                                    for t_dash in self.times
+                                ]
+                            )
+                        )
+                        + self.dict_p[j]
+                        for j in self.jobs
+                    ]
+                ],
+            }
+        )
+
+        # ガントチャートの表示
+        fig = px.timeline(
+            gantt_chart_df,
+            x_start="Start",
+            x_end="Finish",
+            y="Task",
+            color="Task",
+            title="Job Shop Scheduling",
+        )
+        fig.update_yaxes(autorange="reversed")
+        save_name = "gantt_chart_sample"
+        pio.orca.config.executable = "/Applications/orca.app/Contents/MacOS/orca"
+        # 画像の保存
+        # 保存場所はresult/model_1
+        pio.write_image(fig, f"./model/result/model_3/{save_name}.png")
+
+    def get_time(self):
+        """
+        処理時間を取得する関数
+        """
+        return self.model.solutionTime
+
+    def get_model_info(self):
+        """
+        モデルの情報を取得する関数
+        """
+        return pulp.LpStatus[self.status], self.model.objective.value()
+
+    def get_job_order(self):
+        """
+        ジョブの順番を取得する関数
+        """
+        # ジョブをスタート時間でソート
+        job_order = sorted(
+            [
+                [
+                    j,
+                    pulp.value(
+                        pulp.lpSum(
+                            [
+                                t_dash * self.var_z[j, t_dash].value()
+                                for t_dash in self.times
+                            ]
+                        )
+                    ),
+                ]
+                for j in self.jobs
+            ],
+            key=lambda x: x[1],
+        )
+
+        return job_order
--- model/opt_plan_2.py
+++ model/opt_plan_2.py
@@ -1,26 +1,56 @@
 """model_2.pyをimport"""
+import matplotlib.pyplot as plt
 from model_2 import ProdPlan
 
 
-def main(jobs, time_p, weights, time_r):
+def main(nums, indpath):
     """
     最適化問題を実行し、結果を表示する関数
     """
-    prod_plan = ProdPlan(jobs, time_p, weights, time_r)
-    prod_plan.modeling()
-    prod_plan.solve()
-    prod_plan.show_result()
+    # データのパスの指定
+    ind_path = indpath
+    # データを読み込む
+    list_j, list_p, list_w, list_r = ProdPlan.pandas_read(ind_path, nums)
+    # データを辞書型に変換する
+    dict_p, dict_w, dict_r = ProdPlan.list_to_dict(list_j, list_p, list_w, list_r)
+    # モデルのインスタンスを作成する
+    prodplan = ProdPlan(list_j, dict_p, dict_w, dict_r)
+    # モデルを最適化する
+    prodplan.modeling()
+    # モデルの求解
+    prodplan.solve()
+    # ガントチャートで可視化する
+    prodplan.visualize()
+    # 計算時間を表示する
+    time = prodplan.get_time()
+    # モデルの情報を表示する
+    status, object_value = prodplan.get_model_info()
+    # 結果を取得する
+    job_order = prodplan.get_job_order()
+    return status, object_value, time, job_order
 
 
 if __name__ == "__main__":
-    # 初期設定を与える
-    # ジョブの集合
-    Jobs = [1, 2, 3, 4]
-    # ジョブの処理時間の集合
-    Pro_time = [1, 93, 26, 30]
-    # ジョブの重要度の集合
-    Weights = [1, 3, 3, 5]
-    # ジョブのリリース時間の集合
-    Release_time = [63, 4, 63, 99]
-    p, w, r = ProdPlan.make_data(Jobs, Pro_time, Weights, Release_time)
-    main(Jobs, p, w, r)
+    # ジョブ数を変えて計算時間の変化をみる
+    time_list = []
+    job_order_list = []
+    num_set = range(3, 5)
+
+    for i in num_set:
+        status, object_value, time, job_order = main(i, "./model/data/")
+        time_list.append(time)
+        job_order_list.append(job_order)
+
+    # resultに計算時間のグラフを保存する
+    plt.plot(num_set, time_list)
+    plt.xlabel("Number of jobs")
+    plt.ylabel("Time [s]")
+    plt.savefig("./model/result/model_2/time.pdf", format="pdf")
+    # resultに計算時間を保存する
+    with open("./model/result/model_2/time.csv", "w") as f:
+        for i in num_set:
+            f.write(str(i) + "," + str(time_list[i - 3]) + "\n")
+    # resultにジョブの順序を保存する
+    with open("./model/result/model_2/job_order.csv", "w") as f:
+        for i in num_set:
+            f.write("model" + str(i) + "," + str(job_order_list[i - 3]) + "\n")
